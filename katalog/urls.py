2a37b5c6301d28a22c8c13ff57b033122e26bae2
mhmmd-attar <mohammad.attar.w@gmail.com>
2022-09-13 17:39:21 +0700

    Update urls.py of katalog

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementInclude import include
 from django.urls import path
 from katalog.views import show_katalog
 
@@ -5,4 +6,5 @@ app_name = 'katalog'
 
 urlpatterns = [
     path('', show_katalog, name='show_katalog'),
+    path('katalog/', include('katalog.urls')),
 ]
