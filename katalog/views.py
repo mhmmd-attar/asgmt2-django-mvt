4d4bb8cdb6c0cfa2320c4e215396b82435ef44d5
mhmmd-attar <mohammad.attar.w@gmail.com>
2022-09-13 10:57:44 +0700

    Updated the katalog app

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_wishlist_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_wishlist_item,
+        'name': 'Mohammad Attar',
+        'npm': '2106657992',
+    }
+    return render(request, "katalog.html", context)
